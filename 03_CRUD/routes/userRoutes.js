51c8b6b86c82aec0997a4247b7e9aba967c27339
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-04 17:21:00 +0530

    routing

--- 03_CRUD/routes/userRoutes.js
+++ 03_CRUD/routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/", home)
 router.post('/createuser', createUser)
 router.get('/getusers', getUsers)
-router.put('/edituser/:id', editUser)
+router.put('/edituser/:id',  editUser)
 router.delete('/deleteuser/:id', deleteUser)
 
 module.exports = router
